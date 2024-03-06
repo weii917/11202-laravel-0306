42c9f28a4d2b62f31c552f0d2d6258988e75407d
wei <lin951016@gmail.com>
2024-03-06 16:35:34 +0800

    增加navbar在母版

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,25 @@
 
 <head>
     <title>App Name - @yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-    @section('sidebar')
-        This is the master sidebar.
-    @show
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('cats.index') }}">Cats</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('dogs.index') }}">Dogs</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container">
         @yield('content')
