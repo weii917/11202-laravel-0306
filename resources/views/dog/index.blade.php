8e684c8d7626f1cdef655b72fd6adbb47a7304fb
wei <lin951016@gmail.com>
2024-03-06 08:57:50 +0800

    init

---
+++ resources/views/dog/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('cats.index') }}">Cats</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('dogs.index') }}">Dogs</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-3">
+        <h2>Dog List</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>operate</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Amy</td>
+                    <td>
+                        <a href="http://">Edit</a> &nbsp;&nbsp;&nbsp;
+                        <a href="http://">Del</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Amy</td>
+                    <td>
+                        <a href="http://">Edit</a> &nbsp;&nbsp;&nbsp;
+                        <a href="http://">Del</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Amy</td>
+                    <td>
+                        <a href="http://">Edit</a> &nbsp;&nbsp;&nbsp;
+                        <a href="http://">Del</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Amy</td>
+                    <td>
+                        <a href="http://">Edit</a> &nbsp;&nbsp;&nbsp;
+                        <a href="http://">Del</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $(document).ready(function() {
+
+        });
+    </script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DogController;
+use App\Http\Controllers\CatController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // 1.預設近來是到welcome
+    // return view('welcome');
+
+    // 2.redirect 設定直接進哪個頁面
+    return redirect('/cats');
+});
+// 1.網址輸入localhost/dogs執行 DogController
+Route::resource('dogs', DogController::class);
+Route::resource('cats', CatController::class);
+// 2.在CatController自定義function，如何連結到自定義的function輸入cats_excel
+// 做連結要指定->name cats代表controller ，.excel是controller裡定義的excel function
+Route::get('cats_excel', [CatController::class, 'excel'])->name('cats.excel');
